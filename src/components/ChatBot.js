44d8d1ee057c1297f9c1765c82d2d57fc39e4176
210417027 <210417027@gms.tku.edu.tw>
2024-05-22 17:01:53 +0800

    update line chat

--- src/components/ChatBot.js
+++ src/components/ChatBot.js
@@ -1,33 +1,55 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
+import './chatbot.css'; // 引入 CSS 文件
 
-
-const ChatBot = () => {
-    const MessengerRef = useRef();
-    useEffect(() => {
-        MessengerRef.current.setAttribute('page_id', '187860644404201');
-        MessengerRef.current.setAttribute('attribution', 'biz_inbox');
-    
-        window.fbAsyncInit = function () {
-            window.FB.init({
-                xfbml: true,
-                version: 'v16.0',
-            });
-        };
-        (function (d, s, id) {
-            var js,
-                fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) return;
-            js = d.createElement(s);
-            js.id = id;
-            js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-            fjs.parentNode.insertBefore(js, fjs);
-        })(document, 'script', 'facebook-jssdk');
-    }, []);
+const ChatbotButton = () => {
     return (
-        <>
-            <div id="fb-root"></div>
-            <div ref={MessengerRef} id="fb-customer-chat" className="fb-customerchat"></div>
-        </>
+        <a href="https://line.me/R/ti/p/%40983foizr" target="_blank" rel="noopener noreferrer" className="floating-button">
+            <img src="https://xuwztydqqoyqwrjsgfbd.supabase.co/storage/v1/object/public/demo-27/menu_project_img/line.png" alt="LINE" />
+        </a>
     );
-    }; 
-    export default ChatBot;
+};
+
+export default ChatbotButton;
+
+
+
+
+
+
+
+
+
+
+// import React, { useRef, useEffect } from 'react';
+
+
+// const ChatBot = () => {
+//     const MessengerRef = useRef();
+//     useEffect(() => {
+//         MessengerRef.current.setAttribute('page_id', '187860644404201');
+//         MessengerRef.current.setAttribute('attribution', 'biz_inbox');
+    
+//         window.fbAsyncInit = function () {
+//             window.FB.init({
+//                 xfbml: true,
+//                 version: 'v16.0',
+//             });
+//         };
+//         (function (d, s, id) {
+//             var js,
+//                 fjs = d.getElementsByTagName(s)[0];
+//             if (d.getElementById(id)) return;
+//             js = d.createElement(s);
+//             js.id = id;
+//             js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+//             fjs.parentNode.insertBefore(js, fjs);
+//         })(document, 'script', 'facebook-jssdk');
+//     }, []);
+//     return (
+//         <>
+//             <div id="fb-root"></div>
+//             <div ref={MessengerRef} id="fb-customer-chat" className="fb-customerchat"></div>
+//         </>
+//     );
+//     }; 
+//     export default ChatBot;
